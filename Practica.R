8473b600ed587f7c91c65ff085fa6dc17bd724ee
unknown <gaizka.sanmiguel@alumni.mondragon.edu>
2025-06-04 09:15:20 +0200

    Exploracion y limpieza de datos ejercicio 5

--- Practica.R
+++ Practica.R
@@ -17,12 +17,15 @@ sd(df$Seasons, na.rm = T)
 
 df_type <- df %>%
   group_by(type) %>%
-  summary(min_n = mean(Minutes, na.rm = T))
-
+  summarise(min_n = mean(Minutes, na.rm = T))
 df_country <- df %>%
   group_by(country) %>%
-  summary(min_n = mean(Minutes, na.rm = T))
-
+  summarise(min_n = mean(Minutes, na.rm = T))
 df_director <- df %>%
   group_by(director) %>%
-  summary(min_n = mean(Minutes, na.rm = T))
+  summarise(min_n = mean(Minutes, na.rm = T))
+
+#Limpieza
+miss_var_summary(df)
+vis_miss(df)
+is.na(df$Seasons) <- mean(df$Seasons, na.rm = T)
